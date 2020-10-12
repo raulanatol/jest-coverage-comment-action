2d8a3cbebfe07c550f7631f9d75ee7385c44303e
raulanatol <raul@natol.es>
2020-10-12 11:32:19 +0100

    Testing

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ const createComment = async (comment: string) => {
   const octokit = getOctokit(getInput('github-token'));
   const issueNumber = getIssueNumber(context.payload);
   if (!issueNumber) {
-    console.log('PAYLOAD', context.payload);
+    console.log('CONTEXT', context);
     warning('Issue number not found. Impossible to create a comment');
     return;
   }
