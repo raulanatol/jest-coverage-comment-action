66e801fb48a671766b819fb1af75cf5dd0e94ef2
Iván Navarro <ivan@zazume.com>
2022-05-08 20:08:43 +0100

    [compare-converage] Increase version

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,5 @@ import { info, setFailed } from '@actions/core';
 import { start } from './action';
 
 start()
-  .then(() => info('Finished! V0.1.1.1'))
+  .then(() => info('Finished! V0.1.1.2'))
   .catch(error => setFailed(error.message));
